0f943dd449077f4ff4b6df0d536f3809ab5c8de6
Vishal Gupta <72309826+v889@users.noreply.github.com>
2023-07-16 21:14:59 +0530

    Update index.tsx

--- src/scenes/OurClasses/index.tsx
+++ src/scenes/OurClasses/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { ClassType, SelectedPage } from '../../shared/types'
 import   Image1 from "../../assets/image1.png"
 import   Image2 from "../../assets/image2.png"
